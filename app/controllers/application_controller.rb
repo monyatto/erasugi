9725d745bc127c3654c286ff0e70f445b80538e6
monyatto <atsuuu.0925@gmail.com>
2023-09-19 19:01:24 +0900

    ユーザーに関するメニューを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    # ログアウト後に遷移するpathを設定
+  end
 end
