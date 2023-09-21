0d75fb9936528432009fe6e1a7bc49e901c484ad
monyatto <atsuuu.0925@gmail.com>
2023-09-21 18:10:04 +0900

    rubocopの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     new_post_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     new_post_path
   end
 end
