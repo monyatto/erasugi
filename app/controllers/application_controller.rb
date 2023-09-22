d31273aa7da783f9b72734dfcd156db293019b8b
monyatto <atsuuu.0925@gmail.com>
2023-09-22 19:19:12 +0900

    rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(_resource)
     new_post_path
   end
 
   def after_sign_out_path_for(_resource_or_scope)
     new_post_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations'
-  }
+  devise_for :users
   resources :users, only: [:index, :show]
   resources :posts
 end
