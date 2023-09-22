4c19690b42bf5b28342ff5cb6805c43bcf3cd186
monyatto <atsuuu.0925@gmail.com>
2023-09-22 18:43:35 +0900

    ユーザー関連を日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Erasugi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.i18n.default_locale = :ja
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
