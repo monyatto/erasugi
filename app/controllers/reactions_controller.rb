7f3e52898e9e41fb64d01aadb92b456b0a26a57c
monyatto <atsuuu.0925@gmail.com>
2023-10-03 15:16:18 +0900

    不要な記述を削除

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -5,10 +5,4 @@ class ReactionsController < ApplicationController
     @reaction = Reaction.new(post_id: params[:post_id], reactions_type_id: params[:reactions_type_id])
     redirect_to post_path(params[:post_id]) if @reaction.save!
   end
-
-  # def destroy
-  #   @reaction = Reaction.find_by(post_id: params[:post_id], reactions_types_id: params[:reactions_types_id])
-  #   @reaction.destroy
-  #   redirect_to post_path(params[:post_id])
-  # end
 end
