379ccdb552ebf12f0452b4251bfa88c377df3e45
monyatto <atsuuu.0925@gmail.com>
2023-10-11 21:05:46 +0900

    TailwindとDaisyUIを入れた

---
+++ config/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Zen Maru Gothic', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["wireframe"]
+  }
+}
