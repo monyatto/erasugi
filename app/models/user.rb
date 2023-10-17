ff73bc001597cf9afa114706ca07b5c7762f471f
monyatto <atsuuu.0925@gmail.com>
2023-10-17 14:52:20 +0900

    Konvaでのサンプルアニメを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
 
-  generate_public_uid  generator: PublicUid::Generators::HexStringSecureRandom.new(20)
+  generate_public_uid generator: PublicUid::Generators::HexStringSecureRandom.new(20)
 
   def to_param
     public_uid
