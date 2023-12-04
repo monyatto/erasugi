33c95ab12c62767ab71abaa29ff575f4d5daa3a8
monyatto <atsuuu.0925@gmail.com>
2023-12-04 16:54:37 +0900

    スクリーンショット機能の追加

---
+++ app/javascript/controllers/screenshot_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+import Html2canvas from 'html2canvas';
+
+export default class extends Controller {
+    connect() {
+        this.element.addEventListener('click', this.takeScreenshot.bind(this));
+    }
+
+    takeScreenshot() {
+        Html2canvas(document.body).then(canvas => {
+            const link = document.createElement('a');
+            link.download = 'screenshot.png';
+            link.href = canvas.toDataURL();
+            link.click();
+        });
+    }
+}
