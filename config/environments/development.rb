59e48926f1b62efcc6e255da307277b829601c87
monyatto <atsuuu.0925@gmail.com>
2024-01-20 13:20:34 +0900

    パスワード再設定用のメールを確認。日本語化。

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener_web
 end
