7cc36eb254bbf7ab51a3b60b3643c5bc8f9be007
monyatto <atsuuu.0925@gmail.com>
2024-01-20 11:48:14 +0900

    インデント調節

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,29 +8,29 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
-      extend: {
-          fontFamily: {
-              sans: ['Zen Maru Gothic', ...defaultTheme.fontFamily.sans],
-          },
-          keyframes: {
-              disappear: {
-                  "0%": {opacity: 1},
-                  "100%": {opacity: 0},
-              },
-          },
-          animation: {
-              disappear: "disappear 3s ease 0s 1 forwards",
-          },
+    extend: {
+      fontFamily: {
+        sans: ['Zen Maru Gothic', ...defaultTheme.fontFamily.sans],
       },
-  },
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/container-queries'),
-        require("daisyui")
-    ],
-    daisyui: {
-        themes: ["wireframe"]
+      keyframes: {
+        disappear: {
+          "0%": {opacity: 1},
+          "100%": {opacity: 0},
+        },
+      },
+      animation: {
+        disappear: "disappear 3s ease 0s 1 forwards",
+      },
     },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: ["wireframe"]
+  },
 }
