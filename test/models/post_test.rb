c8924c1b27f5af7a29c87dbab68e93db91c16a07
monyatto <atsuuu.0925@gmail.com>
2024-01-22 15:41:29 +0900

    不要なテストファイルを削除、すべてのテストが通るようにした

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -12,12 +12,12 @@ class PostTest < ActiveSupport::TestCase
   end
 
   test '#associated_reaction_type_ids' do
-    reactions_type_ids = (1..101).to_a
+    reactions_type_ids = [1, 2, 3].cycle.take(101)
     reactions_type_ids.each do |id|
       @post.reactions.create(reactions_type_id: id)
     end
 
-    assert_equal (2..101).to_a.reverse, @post.associated_reaction_type_ids,
+    assert_equal reactions_type_ids.reverse.shift(100), @post.associated_reaction_type_ids,
                  'Expected the last 100 reaction type IDs in reverse order, but got a different result.'
   end
 end
