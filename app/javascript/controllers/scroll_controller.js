2a2e3628a7cc72ee012c778be313739ae229a023
monyatto <atsuuu.0925@gmail.com>
2024-02-14 12:09:32 +0900

    ブラウザの戻るボタンでリアクションが表示されないのを修正した

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,29 +1,31 @@
-// ファイル2
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 import Swiper from 'swiper';
 
 export default class extends Controller {
-    static values = { postsIds: Array }
+  static values = {postsIds: Array}
 
-    connect() {
-        this.swiper = new Swiper(".swiper", {
-            direction: 'vertical',
-            mousewheel: {
-                thresholdDelta: 100,
-            },
-            speed: 400,
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-            on: {
-                slideChange: () => {
-                    const activeIndex = this.swiper.activeIndex;
-                    const activePostId = this.postsIdsValue[activeIndex];
-                    const event = new CustomEvent('postIdChanged', {detail: String(activePostId)});
-                    window.dispatchEvent(event);
-                },
-            },
-        });
-    }
+  connect() {
+    this.swiper = new Swiper(".swiper", {
+      direction: 'vertical',
+      mousewheel: {
+        thresholdDelta: 100,
+      },
+      speed: 400,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      on: {
+        slideChange: () => {
+          const activeIndex = this.swiper.activeIndex;
+          const activePostId = this.postsIdsValue[activeIndex];
+          const event = new CustomEvent('postIdChanged', {detail: String(activePostId)});
+          window.dispatchEvent(event);
+        },
+      },
+    });
+    window.onpopstate = function (event) {
+      location.reload();
+    };
+  }
 }
