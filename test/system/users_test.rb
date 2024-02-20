f07f7d65e634e20ca43f3c23370bca0664ba5eb4
monyatto <atsuuu.0925@gmail.com>
2024-02-20 12:44:44 +0900

    ユーザー作成と削除にシステムテストを追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -23,4 +23,28 @@ class UsersTest < ApplicationSystemTestCase
     assert_current_path root_path
     assert_selector('#flash-message', text: 'ログアウトしました')
   end
+
+  test 'create user' do
+    visit new_user_registration_path
+    fill_in 'Eメール', with: 'test3@example.com'
+    fill_in 'ユーザー名', with: 'テスト三郎'
+    fill_in 'パスワード ※6文字以上', with: 'password'
+    fill_in 'パスワード（確認用）', with: 'password'
+    click_on '登録する'
+
+    assert_current_path root_path
+    assert_selector('#flash-message', text: 'アカウント登録が完了しました')
+  end
+
+  test 'destroy user' do
+    user = users(:test_user1)
+    sign_in user
+    visit edit_user_registration_path(user.id)
+    accept_alert do
+      click_on 'アカウントを削除する'
+    end
+
+    assert_current_path root_path
+    assert_selector('#flash-message', text: 'アカウントを削除しました')
+  end
 end
