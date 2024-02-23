e4f4291617c17ea81fc7d29cc8594776ea976171
monyatto <atsuuu.0925@gmail.com>
2024-02-23 22:35:05 +0900

    リアクションにシステムテストを追加

---
+++ test/system/reactions_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+class ReactionsTest < ApplicationSystemTestCase
+  setup do
+    @post = posts(:test_post1)
+    @reaction_type = reactions_types(:test_reactions_type1)
+    ActionController::Base.allow_forgery_protection = true
+  end
+
+  teardown do
+    ActionController::Base.allow_forgery_protection = false
+  end
+
+  test "create reaction" do
+    visit post_path(@post.public_uid)
+    assert_difference '@post.reactions.count' do
+      sleep 1
+      click_on @reaction_type.name
+      sleep 1
+    end
+  end
+end
+
