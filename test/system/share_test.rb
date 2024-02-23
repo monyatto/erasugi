6fb3cf7a303e3eb768be788b945144a46bae7e0a
monyatto <atsuuu.0925@gmail.com>
2024-02-23 12:49:45 +0900

    lineのshareボタンのシステムテストを追加

--- test/system/share_test.rb
+++ test/system/share_test.rb
@@ -5,15 +5,24 @@ require 'application_system_test_case'
 class SocialSharingTest < ApplicationSystemTestCase
   setup do
     @user = users(:test_user1)
-    @post1 = posts(:test_post1)
+    @post = posts(:test_post1)
   end
 
   test 'share to x' do
     sign_in @user
-    visit post_path(@post1.public_uid)
+    visit post_path(@post.public_uid)
     find('#x-share').click
 
     switch_to_window(windows.last)
-    assert_equal current_url, "https://twitter.com/intent/tweet?text=#{ERB::Util.url_encode(@post1.content)}%20#{post_url(@post1)}%20%E3%81%88%E3%82%89%E3%81%99%E3%81%8E"
+    assert_equal current_url, "https://twitter.com/intent/tweet?text=#{ERB::Util.url_encode(@post.content)}%20#{post_url(@post)}%20%E3%81%88%E3%82%89%E3%81%99%E3%81%8E"
+  end
+
+  test 'share to line' do
+    sign_in @user
+    visit post_path(@post.public_uid)
+    find('#line-share').click
+
+    switch_to_window(windows.last)
+    assert_equal current_url, "https://social-plugins.line.me/lineit/share?url=#{post_url(@post)}"
   end
 end
