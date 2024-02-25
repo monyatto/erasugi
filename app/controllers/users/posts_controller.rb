cafb0651207cd0ba98d7428e7f5c7349695892e2
monyatto <atsuuu.0925@gmail.com>
2024-02-25 13:11:57 +0900

    権限が必要なページに権限がない状態でアクセスした場合の処理を修正

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::PostsController < ApplicationController
+  before_action :set_user, only: %i[index]
   before_action :correct_user, only: %i[index]
 
   def index
@@ -9,9 +10,16 @@ class Users::PostsController < ApplicationController
 
   private
 
+  def set_user
+    @user = User.find_by(public_uid: params[:id])
+  end
+
   def correct_user
-    @user = User.find_by(public_uid: params[:user_id])
-    redirect_to(root_url) unless current_user?(@user)
+    if current_user.nil?
+      redirect_to(new_user_session_path)
+    elsif !current_user?(@user)
+      render file: Rails.root.join('public/404.html'), status: 404, layout: false, content_type: 'text/html'
+    end
   end
 
   def current_user?(user)
