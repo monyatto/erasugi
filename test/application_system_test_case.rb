031843725a25a1fb13dbe0109aab2b0d83e0eadf
monyatto <atsuuu.0925@gmail.com>
2024-02-28 18:37:28 +0900

    ヘッドレスブラウザの設定を追加

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,5 +4,12 @@ require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include Devise::Test::IntegrationHelpers
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV['HEADED']
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  else
+    driven_by(:selenium, using: :headless_chrome) do |driver_option|
+      driver_option.add_argument('--no-sandbox')
+      driver_option.add_argument('--disable-dev-shm-usage')
+    end
+  end
 end
