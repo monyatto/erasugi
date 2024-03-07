ddadb3c899a18d4111accc6308a8e373eee4c8bf
monyatto <atsuuu.0925@gmail.com>
2024-03-07 19:19:24 +0900

    eslintの指摘箇所を修正

--- app/javascript/controllers/form_character_count_controller.js
+++ app/javascript/controllers/form_character_count_controller.js
@@ -1,25 +1,25 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from '@hotwired/stimulus'
 
 // Connects to data-controller="form-character-count"
 export default class extends Controller {
-  connect() {
-    const textArea = document.getElementById("post_content");
-    const textLength = document.getElementById("text-length");
-    const textLimit = 140;
+  connect () {
+    const textArea = document.getElementById('post_content')
+    const textLength = document.getElementById('text-length')
+    const textLimit = 140
     if (textArea) {
-      textArea.addEventListener("keyup", function () {
-        const count = textArea.value.length;
+      textArea.addEventListener('keyup', function () {
+        const count = textArea.value.length
         if (count <= textLimit) {
-          if (textArea.classList.contains("err")) {
-            textArea.classList.remove("err");
+          if (textArea.classList.contains('err')) {
+            textArea.classList.remove('err')
           }
           textLength.innerHTML =
-            "残り" + (textLimit - count) + "文字入力できます";
+            '残り' + (textLimit - count) + '文字入力できます'
         } else {
-          textArea.classList.add("err");
-          textLength.innerHTML = count - textLimit + "文字オーバーしています";
+          textArea.classList.add('err')
+          textLength.innerHTML = count - textLimit + '文字オーバーしています'
         }
-      });
+      })
     }
   }
 }
--- app/javascript/controllers/screenshot_controller.js
+++ app/javascript/controllers/screenshot_controller.js
@@ -1,17 +1,17 @@
-import { Controller } from "@hotwired/stimulus";
-import Html2canvas from "html2canvas";
+import { Controller } from '@hotwired/stimulus'
+import Html2canvas from 'html2canvas'
 
 export default class extends Controller {
-  connect() {
-    this.element.addEventListener("click", this.takeScreenshot.bind(this));
+  connect () {
+    this.element.addEventListener('click', this.takeScreenshot.bind(this))
   }
 
-  takeScreenshot() {
+  takeScreenshot () {
     Html2canvas(document.body).then((canvas) => {
-      const link = document.createElement("a");
-      link.download = "screenshot.png";
-      link.href = canvas.toDataURL();
-      link.click();
-    });
+      const link = document.createElement('a')
+      link.download = 'screenshot.png'
+      link.href = canvas.toDataURL()
+      link.click()
+    })
   }
 }
