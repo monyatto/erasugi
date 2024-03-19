ea58ec08335aacc66b1d742d6474a006b0cffa57
monyatto <atsuuu.0925@gmail.com>
2024-03-19 22:42:10 +0900

    指定するファイルを追加

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
     "./app/views/**/*.{erb,haml,html,slim}",
-    "./app/views/layouts/application.html.erb"
+    "./app/views/layouts/application.html.erb",
+    "./app/views/layouts/_header.html.erb"
   ],
   theme: {
     extend: {
