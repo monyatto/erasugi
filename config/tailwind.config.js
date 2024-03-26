0819b07e051ee21da0a0bbf710e541523632116f
monyatto <atsuuu.0925@gmail.com>
2024-03-26 18:24:00 +0900

    daisyUiなしでハンバーガーメニューを実装

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -2,32 +2,32 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-  './public/*.html',
-  './app/helpers/**/*.rb',
-  './app/javascript/**/*.js',
-  './app/views/**/*',
-],
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,html,html.erb}"
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Zen Maru Gothic", ...defaultTheme.fontFamily.sans],
+        sans: ["Zen Maru Gothic", ...defaultTheme.fontFamily.sans]
       },
       keyframes: {
         disappear: {
           "0%": { opacity: 1 },
-          "100%": { opacity: 0 },
-        },
+          "100%": { opacity: 0 }
+        }
       },
       animation: {
-        disappear: "disappear 3s ease 2s 1 forwards",
-      },
-    },
+        disappear: "disappear 3s ease 2s 1 forwards"
+      }
+    }
   },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
-    require("@tailwindcss/container-queries"),
+    require("@tailwindcss/container-queries")
   ]
 };
 
