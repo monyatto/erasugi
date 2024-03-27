757b564ae24caf6e2e4613835933d0aa9a0fe0f0
monyatto <atsuuu.0925@gmail.com>
2024-03-27 16:36:35 +0900

    index.jsに記述を追加してテスト

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,8 @@
 
 import { application } from "controllers/application";
 
+import ClipboardController from "./clipboard_controller.js";
+
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
 eagerLoadControllersFrom("controllers", application);
@@ -9,3 +11,5 @@ eagerLoadControllersFrom("controllers", application);
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+
+application.register("clipboard", ClipboardController);
