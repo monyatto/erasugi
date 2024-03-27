107f0263ffe492fb1a3d2ac6a154377265ffd6f5
monyatto <atsuuu.0925@gmail.com>
2024-03-27 20:44:34 +0900

    パスを変更

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,7 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application";
-
-import ClipboardController from "./clipboard_controller.js";
+import { application } from "application";
+import ClipboardController from "clipboard_controller";
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
