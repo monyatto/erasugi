d2ea78ff497d74285f3bee5a20d764698e2a52f5
monyatto <atsuuu.0925@gmail.com>
2024-03-28 17:34:01 +0900

    importmap.rbのcontrollerのpreloadをtrueにした

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,9 +3,8 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-
-pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers", preload: true
 
 pin "konva", to: "https://ga.jspm.io/npm:konva@9.2.2/lib/index.js"
 pin "swiper", to: "https://ga.jspm.io/npm:swiper@11.0.4/swiper.mjs"
