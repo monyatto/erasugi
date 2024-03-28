3c81a03bc2b1d6bccde83fe02b4e5c1ea551b224
monyatto <atsuuu.0925@gmail.com>
2024-03-28 11:39:26 +0900

    preload: trueをなくしてみる

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,7 +3,7 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 
