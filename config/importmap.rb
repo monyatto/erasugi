e3e16aef990460b7463542ee6c214881ab2fb0bb
monyatto <atsuuu.0925@gmail.com>
2024-03-28 18:02:29 +0900

    importmap.rbを書き換え

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers", preload: true
+pin "layouts/application", to: "controllers/hamburger_controller.js"
 
 pin "konva", to: "https://ga.jspm.io/npm:konva@9.2.2/lib/index.js"
 pin "swiper", to: "https://ga.jspm.io/npm:swiper@11.0.4/swiper.mjs"
