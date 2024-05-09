cb0621b8811b36da3e1dbb35fa7a0c744ba1d105
monyatto <atsuuu.0925@gmail.com>
2024-05-09 22:53:08 +0900

    prettier

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -4,7 +4,6 @@ export default class extends Controller {
   static targets = ["url"];
 
   copy(event) {
-    console.log("コントローラー接続")
     event.preventDefault();
     navigator.clipboard.writeText(this.urlTarget);
     this.flashMessage("URLをコピーしました");
