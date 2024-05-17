d8824a9b29f0871c1e5ff1395a78f6fd5b31daee
monyatto <atsuuu.0925@gmail.com>
2024-05-17 13:55:21 +0900

    リアクションのテストを追加

--- test/system/reactions_test.rb
+++ test/system/reactions_test.rb
@@ -4,7 +4,8 @@ require 'application_system_test_case'
 
 class ReactionsTest < ApplicationSystemTestCase
   setup do
-    @post = posts(:test_post1)
+    @first_post = posts(:test_post1)
+    @second_post = Post.last
     ActionController::Base.allow_forgery_protection = true
   end
 
@@ -12,12 +13,21 @@ class ReactionsTest < ApplicationSystemTestCase
     ActionController::Base.allow_forgery_protection = false
   end
 
-  # test 'create reaction' do
-  #   visit post_path(@post.public_uid)
-  #   assert_difference '@post.reactions_count' do
-  #     find_by_id('loaded', wait: 10)
-  #     click_on 'えらい'
-  #     find_by_id('button-on', wait: 10)
-  #   end
-  # end
+  test 'create reaction in show page' do
+    visit post_path(@first_post.public_uid)
+    assert_difference '@first_post.reactions_count' do
+      find_by_id('loaded', wait: 10)
+      click_on 'えらい'
+      find_by_id('button-on', wait: 10)
+    end
+  end
+
+  test 'create reaction in index page' do
+    visit posts_path
+    assert_difference '@second_post.reactions_count' do
+      find_by_id('loaded', wait: 10)
+      click_on 'えらい'
+      find_by_id('button-on', wait: 10)
+    end
+  end
 end
