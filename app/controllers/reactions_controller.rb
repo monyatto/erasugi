b7082b7f19d78ca0091ad6a49310418071f30ea1
monyatto <atsuuu.0925@gmail.com>
2024-06-05 14:50:43 +0900

    saveに失敗した場合の処理を除外

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -3,6 +3,5 @@
 class ReactionsController < ApplicationController
   def create
     @reaction = Reaction.new(post_id: params[:post_id], created_at: params[:created_at])
-    nil unless @reaction.save!
   end
 end
