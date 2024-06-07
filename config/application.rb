6e2834df69fc274b4819abd44733e835fea0b0ac
monyatto <atsuuu.0925@gmail.com>
2024-06-07 17:49:19 +0900

    コンフリクト解消

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module Erasugi
     config.i18n.default_locale = :ja
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,6 +24,8 @@ module Erasugi
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # preloadに関する警告が出ないための設定
     config.action_view.preload_links_header = false
   end
 end
