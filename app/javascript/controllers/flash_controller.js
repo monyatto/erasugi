fd05fd24c8f4a77285ce07ac1b0747fa99663e34
monyatto <atsuuu.0925@gmail.com>
2024-06-21 15:15:33 +0900

    clipboard_controller.js周辺の修正をした

---
+++ app/javascript/controllers/flash_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["flash", "message"];
+
+  displayMessage() {
+    const message = this.messageTarget;
+    const flash = this.flashTarget;
+
+    message.innerText = "URLをコピーしました";
+    message.className =
+      "animate-disappear flex items-center justify-center bg-white fixed w-screen sm:w-[640px] animate-disappear";
+
+    message.dataset.controller = "removal";
+    message.dataset.action = "animationend->removal#remove";
+
+    flash.appendChild(message);
+    flash.style.display = "block";
+  }
+}
