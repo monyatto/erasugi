2f6ad6a8e78560eec8a98c466161dec1fdd40e1b
monyatto <atsuuu.0925@gmail.com>
2024-06-28 14:03:32 +0900

    表示するテキストをparamsから取得するように変更

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,21 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static values = { text: { type: String } };
-  static targets = ["flash", "message"];
+  static targets = ["container", "element"];
 
-  displayMessage() {
-    const message = this.messageTarget;
-    const flash = this.flashTarget;
+  displayMessage({ params: { text } }) {
+    const flashContainer = this.containerTarget;
+    const flashElement = this.elementTarget;
 
-    message.innerText = this.textValue;
-    message.className =
+    flashElement.innerText = text;
+    flashElement.className =
       "animate-disappear flex items-center justify-center bg-white fixed w-screen sm:w-[640px] animate-disappear";
 
-    message.dataset.controller = "removal";
-    message.dataset.action = "animationend->removal#remove";
+    flashElement.dataset.controller = "removal";
+    flashElement.dataset.action = "animationend->removal#remove";
 
-    flash.appendChild(message);
-    flash.style.display = "block";
+    flashContainer.appendChild(flashElement);
+    flashContainer.style.display = "block";
   }
 }
