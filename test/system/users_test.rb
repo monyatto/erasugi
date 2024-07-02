87764be0f6441cbb6902758161a16d5985f04dcc
monyatto <atsuuu.0925@gmail.com>
2024-07-02 14:32:39 +0900

    testを修正

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -39,21 +39,16 @@ class UsersTest < ApplicationSystemTestCase
     fill_in 'パスワード ※6文字以上', with: 'password'
     fill_in 'パスワード（確認用）', with: 'password'
     click_on '登録する'
-    assert_selector('#flash-message', text: 'アカウント登録が完了しました')
+    assert_selector('#flash-message', text: '本人確認用のメールを送信しました')
   end
 
   test 'update user' do
     sign_in @user
     visit edit_user_registration_path
     fill_in 'Eメール', with: 'change@example.com'
-    fill_in 'ユーザー名', with: 'テスト変更'
     fill_in '現在のパスワード ※必須', with: 'password'
     click_on '更新する'
-    assert_selector('#flash-message', text: 'アカウント情報を変更しました')
-
-    visit user_path(@user.public_uid)
-    assert_text 'change@example.com'
-    assert_text 'テスト変更'
+    assert_selector('#flash-message', text: '変更後のアドレスに本人確認用のメールを送信しました')
   end
 
   test 'delete user' do
