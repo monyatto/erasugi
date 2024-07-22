480da97f97b847649b000140075b3209d325a87e
monyatto <atsuuu.0925@gmail.com>
2024-07-22 12:50:51 +0900

    投稿、編集、削除時にogp画像も作成&削除するように変更

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -10,16 +10,19 @@ class Users::PostsController < ApplicationController
   def edit; end
 
   def update
+    delete_ogp_image
     respond_to do |format|
       if @post.update(post_params)
         format.turbo_stream { flash.now[:notice] = 'えらすぎを更新しました' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
+      create_ogp_image
     end
   end
 
   def destroy
+    delete_ogp_image
     @post.destroy
     respond_to do |format|
       format.turbo_stream { flash.now[:notice] = 'えらすぎを削除しました' }
@@ -38,5 +41,14 @@ class Users::PostsController < ApplicationController
 
   def current_user?(user)
     user == current_user
+  end
+
+  def create_ogp_image
+    @post.image.path
+  end
+
+  def delete_ogp_image
+    path = "public/posts/#{@post.image.path}"
+    FileUtils.rm_f(path)
   end
 end
