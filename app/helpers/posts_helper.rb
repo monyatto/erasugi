771ba635fe5fc6bdd5591a5749e65f6389a6c1aa
monyatto <atsuuu.0925@gmail.com>
2024-07-22 14:50:47 +0900

    テストの修正

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,6 @@
 module PostsHelper
   def count_characters(post)
     content_length = post.content&.length.to_i
-    content_length > Post::TEXT_LIMIT ? "#{content_length - 60}文字オーバーです" : "残り#{Post::TEXT_LIMIT - content_length}文字入力できます"
+    content_length > Post::TEXT_LIMIT ? "#{content_length - Post::TEXT_LIMIT}文字オーバーです" : "残り#{Post::TEXT_LIMIT - content_length}文字入力できます"
   end
 end
--- test/helpers/posts_helper_test.rb
+++ test/helpers/posts_helper_test.rb
@@ -9,14 +9,14 @@ class PostsHelperTest < ActionView::TestCase
     @post = Post.new
   end
 
-  test 'should return remaining characters count when post content length is less than or equal to 60' do
-    @post.content = 'a' * 50
+  test 'should return remaining characters count when post content length is less than or equal to 20' do
+    @post.content = 'a' * 10
 
     assert_equal '残り10文字入力できます', count_characters(@post)
   end
 
-  test 'should return over characters count when post content length is more than 60' do
-    @post.content = 'a' * 70
+  test 'should return over characters count when post content length is more than 20' do
+    @post.content = 'a' * 30
 
     assert_equal '10文字オーバーです', count_characters(@post)
   end
